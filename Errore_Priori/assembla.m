7b60946decafbeba507a35274949be5abb5e02c3
mezzelfo <luca.mezzelfo@gmail.com>
2021-11-19 16:40:25 +0100

    errore priori funzionante e punti vettori colonna

--- Errore_Priori/assembla.m
+++ Errore_Priori/assembla.m
@@ -1,8 +1,6 @@
 function [A,b,A_dirichlet,u_dirichlet,b_neumann] = assembla( ...
     epsilon, ...
-    betax, ...
-    betay, ...
-    gamma, ...
+    beta, ...
     f,...
     bordo_dirichlet,...
     bordo_neumann)
@@ -22,13 +20,22 @@ b_neumann = zeros(ndof,1);
 
 for e=1:geom.nelements.nTriangles
     points_idx = geom.elements.triangles(e,:);
-    coords = geom.elements.coordinates(points_idx,:);
+    coords = geom.elements.coordinates(points_idx,:)';
     % LUCA: la seguente istruzione calcola dxi,dyi per i=1,2,3 t.c.
-    % d(i,1) = dxi e d(i,2) = dyi
+    % d(1,i) = dxi e d(2,i) = dyi
     % .*[1,-1] può non servire a patto di ridefinire i dyi come -dyi
-    d = (coords([3,1,2],:)-coords([2,3,1],:)).*[1,-1];    
+    d = [1;-1].*(coords(:,[3,1,2])-coords(:,[2,3,1]));
     area = geom.support.TInfo(e).Area;
-    center = mean(coords);
+    center = mean(coords,2);
+    
+    mk = 1/3;
+    h = sqrt(max(sum(d.^2,1)));
+    Pe = mk*norm(beta(center))*h/(2*epsilon(center));
+    tau = h/(2*norm(beta(center)));
+    if Pe < 1
+        tau = mk*h^2/(2*epsilon(center));
+    end
+    
     for j=1:3
         jj = geom.pivot.pivot(points_idx(j));
         if jj > 0
@@ -36,17 +43,23 @@ for e=1:geom.nelements.nTriangles
                 kk = geom.pivot.pivot(points_idx(k));
                 if kk > 0
                     A(jj,kk) = A(jj,kk) + ...
-                        epsilon(center)/(4*area)*(d(k,2)*d(j,2)+d(k,1)*d(j,1))+...
-                        1/6*(betax(center)*d(k,2)+betay(center)*d(k,1))+...
-                        gamma(center)*area/12*(1+k==j);
+                        epsilon(center)/(4*area)*d(:,k)'*d(:,j)+...
+                        1/6*beta(center)'*d([2,1],k);
+                    
+                    A(jj,kk) = A(jj,kk) + ...
+                        tau/(4*area)*d([2,1],k)'*beta(center)*beta(center)'*d([2,1],j);
                 else
                     A_dirichlet(jj,-kk) = A_dirichlet(jj,-kk) + ...
-                        epsilon(center)/(4*area)*(d(k,2)*d(j,2)+d(k,1)*d(j,1))+...
-                        1/6*(betax(center)*d(k,2)+betay(center)*d(k,1))+...
-                        gamma(center)*area/12*(1+k==j);
+                        epsilon(center)/(4*area)*d(:,k)'*d(:,j)+...
+                        1/6*beta(center)'*d([2,1],k);
+                    
+                    A_dirichlet(jj,-kk) = A_dirichlet(jj,-kk) + ...
+                        tau/(4*area)*d([2,1],k)'*beta(center)*beta(center)'*d([2,1],j);
                 end
             end
             b(jj) = b(jj) + f(center)/3*area;
+            
+            b(jj) = b(jj) + tau/2*f(center)*beta(center)'*d([2,1],j);
         end
     end
 end
@@ -62,11 +75,11 @@ end
 for e = 1:length(geom.pivot.Ne)
     e = geom.pivot.Ne(e,:);
     points_idx = geom.elements.borders(e(1),[1,2]);
-    coords = geom.elements.coordinates(points_idx,:);
-    lunghezza = sqrt(sum((coords(1,:)-coords(2,:)).^2));
+    coords = geom.elements.coordinates(points_idx,:)';
+    lunghezza = sqrt(sum((coords(:,1)-coords(:,2)).^2));
     marker = e(2);
     gn = bordo_neumann(coords, marker);
-    contribs = lunghezza * [[2, 1];[1, 2]]/6 * gn;
+    contribs = lunghezza * [[2, 1];[1, 2]]/6 * gn';
     jj = geom.pivot.pivot(points_idx);
     b_neumann(jj(jj > 0)) = b_neumann(jj(jj > 0)) + contribs(jj > 0);   
 end
--- Errore_Priori/main.m
+++ Errore_Priori/main.m
@@ -4,17 +4,15 @@ clear all
 
 global geom
 
-epsilon = @(x) 1;
-betax = @(x) 0;
-betay = @(x) 0;
-gamma = @(x) 0;
-f = @(P) 32*(P(1)*(1-P(1))+P(2)*(1-P(2)));
-bordo_dirichlet = @(P, marker) 0;
-bordo_neumann = @(P, marker) -16*(P(:,2)-P(:,2).^2);
+true_sol_handle = @(P) 0+16*P(1,:).*(1-P(1,:)).*P(2,:).*(1-P(2,:));
+grad_true_sol_handle = @(P) [   16*P(2,:).*(1-P(2,:)).*(1-2*P(1,:)); 
+                                16*P(1,:).*(1-P(1,:)).*(1-2*P(2,:))];
 
-true_sol_handle = @(P) 0+16*P(1,:,:).*(1-P(1,:,:)).*P(2,:,:).*(1-P(2,:,:));
-grad_true_sol_handle = @(P) [   16*P(2,:,:).*(1-P(2,:,:)).*(1-2*P(1,:,:)); 
-                                16*P(1,:,:).*(1-P(1,:,:)).*(1-2*P(2,:,:))];
+epsilon = @(x) 1;
+beta = @(x) [10000;2];
+f = @(P) 32*(P(1,:)*(1-P(1,:))+P(2,:)*(1-P(2,:))) + beta(P)'*grad_true_sol_handle(P);
+bordo_dirichlet = @(P, marker) 0;
+bordo_neumann = @(P, marker) -16*(P(2,:)-P(2,:).^2);
 
 
 area_ax = logspace(log10(0.08), log10(0.0002), 12);
@@ -22,9 +20,13 @@ h_ax = sqrt(area_ax);
 errors_ax = 0*area_ax;
 tic
 for i = 1:length(area_ax)
-    generate_triangulation(area_ax(i));
+    
+    filename = append('triangolazione_',num2str(area_ax(i)),'.mat');
+    %generate_triangulation(area_ax(i));
+    %save(filename,'geom')
+    load(filename);
         
-    [A,b,A_dirichlet,u_dirichlet,b_neumann] = assembla(epsilon,betax,betay,gamma,f, bordo_dirichlet, bordo_neumann);
+    [A,b,A_dirichlet,u_dirichlet,b_neumann] = assembla(epsilon,beta,f, bordo_dirichlet, bordo_neumann);
     u = A\(b-A_dirichlet*u_dirichlet+b_neumann);
     
     sol = zeros(length(geom.elements.coordinates),1);
@@ -33,7 +35,7 @@ for i = 1:length(area_ax)
     
     
     [tot_err_inf, tot_err_L2, tot_err_H1] = calcola_errore_priori(true_sol_handle,grad_true_sol_handle,sol);
-    errors_ax(i) = tot_err_inf;
+    errors_ax(i) = tot_err_L2;
 
 end
 toc
